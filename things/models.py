54117c91c245e74aed495f98dce8571f12b5afb1
salahi salim <salahi.salim@outlook.com>
2022-12-09 14:59:05 +0000

    lsofprimes

--- things/models.py
+++ things/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator,MinValueValidator
 
-# Create your models here.
-
-class User(AbstractUser):
-    name = models.CharField(max_length=30, unique=True, blank = False)
-    description = models.CharField(max_length=120,unique = False ,blank = True)
-    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+class Thing(models.Model):
+    name = models.CharField(max_length = 30, unique = True,blank=False)
+    description = models.CharField(max_length = 120, unique = False,blank=True)
+    quantity = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
 
 
 
