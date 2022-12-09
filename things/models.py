b267c2a0c6084888f1a837514ceb6330282aa363
salahi salim <salahi.salim@outlook.com>
2022-12-09 14:45:11 +0000

    user model things1

--- things/models.py
+++ things/models.py
@@ -5,11 +5,11 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class User(AbstractUser):
-    name = models.TextField(max_length=30, unique=True)
+    name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120, blank = True)
     quantity = models.IntegerField(
     default = 1,
-    validators=[MaxValueValidator(100), MinValueValidator(0)]
+    validators=[MinValueValidator(0), MaxValueValidator(100)]
      )
 
 
